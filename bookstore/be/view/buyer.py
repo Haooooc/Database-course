db7b0a6f3a7c3de2764ecbe20e69a6f503cf1ec5
Haooooc <1580513987@qq.com>
2025-04-07 17:42:20 +0800

    update files

--- bookstore/be/view/buyer.py
+++ bookstore/be/view/buyer.py
@@ -40,3 +40,27 @@ def add_funds():
     b = Buyer()
     code, message = b.add_funds(user_id, password, add_value)
     return jsonify({"message": message}), code
+
+
+@bp_buyer.route("/receive_order", methods=["POST"])
+def receive_order():
+    user_id = request.json.get("user_id")
+    order_id = request.json.get("order_id")
+    b = Buyer()
+    code, message = b.receive_order(user_id, order_id)
+    return jsonify({"message": message}), code
+
+@bp_buyer.route("/search_history_order",methods=["POST"])
+def search_history_order():
+    user_id = request.json.get("user_id")
+    b = Buyer()
+    code, message,_ = b.search_history_order(user_id)
+    return jsonify({"message": message}), code
+
+@bp_buyer.route("/cancel_order",methods=["POST"])
+def cancel_order():
+    user_id = request.json.get("user_id")
+    order_id = request.json.get("order_id")
+    b = Buyer()
+    code, message = b.cancel_order(user_id, order_id)
+    return jsonify({"message": message}), code
